cb677c896e88f6956031f211e1d46b84db4ee492
venler.wei <venler.wei@huolala.cn>
2020-07-07 15:04:54 +0800

    1

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -14,9 +14,9 @@ export default {
     window.wx.config({
       debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: 'wx57f1ffbcdc635211', // 必填，公众号的唯一标识
-      timestamp: '1594104671', // 必填，生成签名的时间戳
-      nonceStr: 'tbaixdgfd19', // 必填，生成签名的随机串
-      signature: 'ec9a68224dcc15c4d2f909d90991fd7e00b4a793', // 必填，签名
+      timestamp: '1594105461', // 必填，生成签名的时间戳
+      nonceStr: 'op585wevmu', // 必填，生成签名的随机串
+      signature: '4b4f29310e6029e9b600fbc0573c61ede7d7fd50', // 必填，签名
       jsApiList: ['onMenuShareWeibo'], // 必填，需要使用的JS接口列表
     });
     // // axios.get('http://www.sevenseven.site/wx/').then((res) => {
